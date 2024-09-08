949e085e244287aa8e2602c47590a5fd6cb4b13c
Chatchai Wangwiwattana <redeian@gmail.com>
2024-09-08 14:52:23 +0700

    first commit

---
+++ lab1.py
@@ -0,0 +1,38 @@
+# Challenge!
+# ----------------------------------
+# creating a function to calculate the area of circle.
+# Users will specify the number of circle, 
+# the program will calculate the area of each circle with radius -1
+# for example
+# input : 5
+# process: [the area of circle of radius 1-5 will be calcuated]. 
+# output: [print all areas on the screen]
+
+def cal_circle_area( radius ):
+    return 3.141 * radius * radius
+
+circle_number = int(input("give me number : "))
+
+areas = []
+
+for i in range(1, circle_number+1):
+    area = cal_circle_area(i)
+    areas.append(area)
+
+    print (f"the area of {i} is {area}")
+
+areas.pop(0)
+areas.append(10)
+print(sum(areas))
+
+# Challenge 2!
+# --------------------------------
+# from the first challenge...store all areas in an array. :)
+# take the first area of the list, 
+#                    and then add 10 at the end of the array.
+# Then, sum of all areas and display on screen.
+
+grades = {'Mark': 'A', 
+          'Jib': 'B'}
+
+print(grades['Mark']) # A
